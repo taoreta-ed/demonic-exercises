057e94805b545d088a302669d5dbadc5f1783b4f
Eduardo Hernández Castro <akuma_ed@outlook.com>
2021-01-28 13:58:04 -0600

    first game logic added

--- pptls/app.js
+++ pptls/app.js
@@ -1,3 +1,56 @@
 /* Juego de Piedra, papel, tijeras, lagarto y spock */
 
-let random;
+let op1 = "Piedra";
+let op2 = "Papel";
+let op3 = "Tijeras";
+let op4 = "Lagarto";
+let op5 = "Spock";
+
+let mach;
+
+function getRandom(){
+    return Math.floor(Math.random()*4);
+}
+
+function machineChoice(){
+    let random = getRandom();
+
+    switch(random){
+        case 1: 
+            mach = op1;
+            console.log('Soy la opcion ' + mach);
+            return mach;
+            break;
+        case 2: 
+            mach = op2;
+            console.log('Soy la opcion ' + mach);
+            return mach;
+            break;
+        case 3: 
+            mach = op3;
+            console.log('Soy la opcion ' + mach);
+            return mach;
+            break;
+        case 4: 
+            mach = op4;
+            console.log('Soy la opcion ' + mach);
+            return mach;
+            break;
+    }
+}
+
+function game(player, mach){
+    if(player == mach){
+        console.log('Es un empate');
+    }else if (player == "Piedra" && mach == "Tijeras"){
+        console.log('¡Piedra rompe tijeras! Gana el jugador');
+    }else if (player == "Piedra" && mach == "Papel"){
+        console.log('Gana Tenn');
+    }else if (player == "Piedra" && mach == "Lagarto"){
+        console.log('Gana el jugador');
+    }else if (player == "Piedra" && mach == "Spock"){
+        console.log('Gana Tenn');
+    }else if (player == "Papel" && mach == "Piedra"){
+        console.log('Gana el jugador');
+    }
+}
